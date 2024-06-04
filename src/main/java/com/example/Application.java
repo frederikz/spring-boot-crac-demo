aea686020e8660605545c40b3938e838a4cc7796
Frederik <20817974+frederikz@users.noreply.github.com>
2024-06-04 11:52:26 +0200

    ReactorResourceFactory not working with CRaC

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,12 +2,19 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public WebClient someWebClient(WebClient.Builder webClientBuilder) {
+		return webClientBuilder.build();
 	}
 
 }
