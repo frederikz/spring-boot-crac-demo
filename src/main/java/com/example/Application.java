a3109a32d4355534cd6f5889d334b30191057d8a
Frederik <20817974+frederikz@users.noreply.github.com>
2024-06-07 13:27:17 +0200

    onRefresh triggered twice

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.support.GenericApplicationContext;
 
 @SpringBootApplication
 public class Application {
@@ -13,8 +15,17 @@ public class Application {
 	}
 	
 	@Bean
-	public WebClient someWebClient(WebClient.Builder webClientBuilder) {
-		return webClientBuilder.build();
+	public SomeBean someBean() {
+		return new SomeBean();
 	}
-
+	
+	public static class SomeBean implements ApplicationListener<ContextRefreshedEvent> {
+		
+		@Override
+	    public void onApplicationEvent(ContextRefreshedEvent event) {
+			GenericApplicationContext applicationContext = new GenericApplicationContext();
+			applicationContext.refresh();
+	    }
+	}
+	
 }
